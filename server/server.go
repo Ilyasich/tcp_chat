bbcc4a7c403497feef12a8a04fdd45f03cde63a1
Trevor <87931989+Ilyasich@users.noreply.github.com>
2024-02-10 06:22:31 +0000

    add Makefile

--- server/server.go
+++ server/server.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net"
+
 	"tcp_chat/config"
 )
 
 func StartServer() {
-	listener, err := net.Listen("tcp", "localhost:8000") //создаем TCP-сервер, прослушивающий подключения на локальном адресе и порту 8000
-	fmt.Println("Server has started!")//сообщаем о старте сервера
+	listener, err := net.Listen("tcp", "localhost:8000") //создаем TCP-сервер, прослушивающий подключения на локальном адресе и порту 8080
+
+	fmt.Println("Server has started!") //сообщаем о старте сервера
 	if err != nil {
 		log.Fatal(err)
 	}
 	go config.Broadcaster() //горутина которая будет обрабатывать сообщения и управлять подключенными клиентами.
 	for {
-		conn, err := listener.Accept()//слушаем порт
+		conn, err := listener.Accept() //слушаем порт
 		if err != nil {
 			log.Print(err)
 			continue
