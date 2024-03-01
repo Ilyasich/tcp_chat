3772853ad7b4c7ecab7c07b817d6517f741acaa4
ilyasich <s9179175122@gmail.com>
2024-03-01 15:24:41 +0400

    Add log

--- config/config.go
+++ config/config.go
@@ -6,18 +6,20 @@ import (
 	"net"
 	"strings"
 	"time"
+
+	"go.uber.org/zap"
 )
 
-type client chan<- string //тип клиент
+type client chan<- string
 
 var (
-	entering = make(chan client)//канал для  для регистрации новых клиентов
-	leaving  = make(chan client)//для отслеживания клиентов, которые покидают чат
-	messages = make(chan string)//канал для передачи сообщений между клиентами.
+	entering = make(chan client) //канал для регистрации новых клиентов
+	leaving  = make(chan client) //канал для отслеживания клиентов, которые покидают чат
+	messages = make(chan string) //канал для передачи сообщений между клиентами
 )
 
-func Broadcaster() {
-	clients := make(map[client]bool)//мапа для отслеживания подключенных клиентов
+func Broadcaster(lg *zap.SugaredLogger) {
+	clients := make(map[client]bool) //мапа для отслеживания подключенных клиентов
 	//обработка событий
 	for {
 		select {
@@ -35,17 +37,18 @@ func Broadcaster() {
 }
 
 // обрабатывает подключение клиента к серверу
-func HandleConn(conn net.Conn) {
-	currentTime := time.Now().Format("2006-01-02 15:04:05") //функция времени
+func HandleConn(lg zap.SugaredLogger, conn net.Conn) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
 	conn.Write([]byte(fmt.Sprintf("Welcome to chat! \nData: %s\n \nEnter your nickname:", currentTime))) //вывод на экран времени подключенного клиента
 	reader := bufio.NewReader(conn)
 	nickname, _ := reader.ReadString('\n')
 	nickname = strings.TrimSpace(nickname)
 	fmt.Println("New user:", nickname)
+	lg.Info("New user:", nickname)
 
 	conn.Write([]byte(fmt.Sprintf("Welcome! Data: %s\n", currentTime)))
 
-	ch := make(chan string) //канал для передачи информации о действии клиентов
+	ch := make(chan string)   //канал для передачи информации о действии клиентов
 	go ClientWriter(conn, ch) //горутина пишущая в канал никнеймы сообщения и кто присоеденился и покинул чат
 	ch <- "You are: " + nickname
 	messages <- nickname + " has arrived"
@@ -57,6 +60,7 @@ func HandleConn(conn net.Conn) {
 	leaving <- ch
 	messages <- nickname + " has left chat"
 	fmt.Println(nickname + " has left chat")
+	lg.Info(nickname + "has left chat")
 	conn.Close()
 }
 
--- server/server.go
+++ server/server.go
@@ -1,27 +1,39 @@
 package server
 
 import (
+	"tcp_chat/config"
+
+	"go.uber.org/zap"
+
 	"fmt"
 	"log"
 	"net"
 
-	"tcp_chat/config"
 )
 
 func StartServer() {
-	listener, err := net.Listen("tcp", "localhost:8000") //создаем TCP-сервер, прослушивающий подключения на локальном адресе и порту 8080
 
-	fmt.Println("Server has started!") //сообщаем о старте сервера
+	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatal(err)
 	}
-	go config.Broadcaster() //горутина которая будет обрабатывать сообщения и управлять подключенными клиентами.
+	defer logger.Sync()
+
+	lg := logger.Sugar()
+
+	listener, err := net.Listen("tcp", "localhost:8000")//создаем TCP-сервер, прослушивающий подключения на локальном адресе и порту 8080
+	fmt.Println("Server has started!")
+	lg.Info("Server has started...")
+	if err != nil {
+		lg.Fatal(err)
+	}
+	go config.Broadcaster(lg)//горутина которая будет обрабатывать сообщения и управлять подключенными клиентами.
 	for {
-		conn, err := listener.Accept() //слушаем порт
+		conn, err := listener.Accept()
 		if err != nil {
-			log.Print(err)
+			lg.Panicln("Port is not evelible", err)
 			continue
 		}
-		go config.HandleConn(conn)
+		go config.HandleConn(*lg, conn)
 	}
 }
